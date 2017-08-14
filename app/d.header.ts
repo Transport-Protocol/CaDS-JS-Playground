01feb74a231b6a00054fecbd90b22a8a8ee85e94
scimbe <scimbe@gmail.com>
2017-08-14 21:44:53 +0200

    add some nice things

--- app/d.header.ts
+++ app/d.header.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'cads-header',
   templateUrl: '../templates/cads_header.html'
 })
 
-export class Header { }
+export class Header implements OnInit{
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    console.log("= Create header... =");
+    this.router.events.subscribe((evt) => {
+        if (!(evt instanceof NavigationEnd)) {
+            return;
+        }
+        $('body').animate({ scrollTop: 50 }, 500);
+    });
+  }
+
+ }
