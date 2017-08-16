9703f92008806cc846c3b8c434669afed520a5f3
scimbe <scimbe@gmail.com>
2017-08-16 12:06:56 +0200

    update files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,63 @@
 module.exports = function(grunt) {
 
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
   grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['./**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
-    }
+
+    // all of our configuration will go here
+
+
+ 	uglify: {
+      		options: {
+        		banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      		},
+      		build: {
+        		files: {
+          		'dist/node_modules/@angular/compiler/bundles/compiler.umd.js': 'node_modules/@angular/compiler/bundles/compiler.umd.js',
+          		'dist/node_modules/@angular/core/bundles/core.umd.js': 'node_modules/@angular/core/bundles/core.umd.js',
+          		'dist/node_modules/systemjs/dist/system.src.js': 'node_modules/systemjs/dist/system.src.js',
+          		'dist/node_modules/@angular/forms/bundles/forms.umd.js': 'node_modules/@angular/forms/bundles/forms.umd.js',
+          		'dist/node_modules/@angular/common/bundles/common.umd.js': 'node_modules/@angular/common/bundles/common.umd.js',
+          		'dist/node_modules/@angular/router/bundles/router.umd.js': 'node_modules/@angular/router/bundles/router.umd.js',
+          		'dist/node_modules/@angular/platform-browser/bundles/platform-browser.umd.js': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
+          		'dist/node_modules/reflect-metadata/Reflect.js': 'node_modules/reflect-metadata/Reflect.js',
+          		'dist/node_modules/zone.js/dist/zone.js': 'node_modules/zone.js/dist/zone.js',
+          		'dist/node_modules/rxjs/Observable.js': 'node_modules/rxjs/Observable.js',
+          		'dist/node_modules/rxjs/Subscription.js': 'node_modules/rxjs/Subscription.js',
+          		'dist/node_modules/rxjs/operator/observeOn.js': 'node_modules/rxjs/operator/observeOn.js',
+          		'dist/node_modules/rxjs/operator/reduce.js': 'node_modules/rxjs/operator/reduce.js',
+          		'dist/node_modules/rxjs/operator/concatMap.js': 'node_modules/rxjs/operator/concatMap.js',
+          		'dist/node_modules/rxjs/operator/mergeMap.js': 'node_modules/rxjs/operator/mergeMap.js',
+          		'dist/node_modules/rxjs/operator/filter.js': 'node_modules/rxjs/operator/filter.js',
+          		'dist/node_modules/rxjs/observable/FromObservable.js': 'node_modules/rxjs/observable/FromObservable.js',
+          		'dist/node_modules/rxjs/operator/first.js': 'node_modules/rxjs/operator/first.js',
+          		'dist/node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js': 
+'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+          		'dist/node_modules/rxjs/Subscriber.js': 'node_modules/rxjs/Subscriber.js',
+          		'dist/node_modules/rxjs/operator/catch.js': 'node_modules/rxjs/operator/catch.js',
+          		'dist/node_modules/rxjs/observable/ArrayObservable.js': 'node_modules/rxjs/observable/ArrayObservable.js',
+          		'dist/node_modules/rxjs/operator/map.js': 'node_modules/rxjs/operator/map.js',
+          		'dist/node_modules/rxjs/operator/concatAll.js ': 'node_modules/rxjs/operator/concatAll.js ',
+          		'dist/node_modules/rxjs/operator/mergeAll.js': 'node_modules/rxjs/operator/mergeAll.js',
+          		'dist/node_modules/rxjs/observable/EmptyObservable.js': 'node_modules/rxjs/observable/EmptyObservable.js',
+          		'dist/cads.informatik.haw-hamburg.de/node_modules/rxjs/Notification.js': 'cads.informatik.haw-hamburg.de/node_modules/rxjs/Notification.js',
+          		'node_modules/rxjs/observable/PromiseObservable.js': 'node_modules/rxjs/observable/PromiseObservable.js',
+          		'node_modules/rxjs/operator/last.js': 'node_modules/rxjs/operator/last.js'
+        		}
+		}
+	}
   });
 
+  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
 
 };
 
